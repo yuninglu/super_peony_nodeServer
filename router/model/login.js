38d52ea37e0d7554b6140871e80c24ef70719ee0
yuninglu <1278843795@qq.com>
2021-09-09 11:51:43 +0800

    push Access-Contorl-Allow-Origin

--- router/model/login.js
+++ router/model/login.js
@@ -3,6 +3,7 @@ const loginModel = require("../../model/login");
 async function func(req, res, next) {
   const task = await loginModel.findOne({ username: "yuninglu" });
   console.log(task);
+  res.send({ a: "成功" });
 }
 module.exports = {
   func,
